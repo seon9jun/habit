9a62b28743c07f2f70562916e3cc94925934839c
SJ <hsj9360@gmail.com>
2022-02-01 03:38:01 +0900

    TIL: using react hook form

---
+++ src/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { RecoilRoot } from 'recoil';
+import { ThemeProvider } from 'styled-components';
+import App from './App';
+import { theme } from './theme';
+
+ReactDOM.render(
+	<React.StrictMode>
+		<RecoilRoot>
+			<ThemeProvider theme={theme}>
+				<App />
+			</ThemeProvider>
+		</RecoilRoot>
+	</React.StrictMode>,
+	document.getElementById('root')
+);
